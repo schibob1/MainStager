7d2a254697b75ae141af8e87d8645c4a09fbf3b9
Christoph Fuchs <mp211521@fhstp.ac.at>
2022-04-27 17:22:53 +0200

    Changed Background

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -13,6 +13,7 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
 
+
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
