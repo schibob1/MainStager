ba1094b417ce7c385ef71fa94b50ae308d867e67
steffenacosta <mp211510@fhstp.ac.at>
2022-05-17 16:31:33 +0200

    Review `MainContainer_ParameterIds`, adjust if necessary and then merge (into the develop), because there are also structural changes (directory structure).
    Signed-off-by: steffenacosta <mp211510@fhstp.ac.at>

--- Source/ParameterIds.h
+++ Source/ParameterIds.h
@@ -8,8 +8,14 @@
 
 namespace ParameterIds
 {
+    /*  static const juce::String reverbSize = "reverbSize";
+    static const juce::String reverbDamp = "reverbDamp";
+    static const juce::String reverbWidth = "reverbWidth";
+    static const juce::String reverbDW = "reverbDW";**/
+
     static const juce::String compressorAttack = "compressorAttack";
-
-
+    /*  static const juce::String compressorRelease = "compressorRelease";
+    static const juce::String compressorThreshold = "compressorThreshold";
+    static const juce::String compressorRatio = "compressorRatio"; **/
 
 }
